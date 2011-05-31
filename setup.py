3cf3b68dbba2726d9c28a65b8f092ab3c92afe2c
Nathaniel Tucker <me@ntucker.me>
2011-05-30 19:16:07 -0700

    Adding setup.py for installation.

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Django Recaptcha',
+      version='0.1',
+      description='Recaptcha for Django Forms',
+      author='SmileyChris',
+      url='http://bitbucket.org/ntucker/django-recaptcha',
+      packages=['recaptcha_form'],
+     )
