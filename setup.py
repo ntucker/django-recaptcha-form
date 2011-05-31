96f2bdb839f0d2fddadc3a0698773cd49e040e71
Nathaniel Tucker <me@ntucker.me>
2011-05-31 10:22:37 -0700

    Rearranging where credit is given

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 setup(name='Django Recaptcha',
       version='0.1',
       description='Recaptcha for Django Forms',
-      author='SmileyChris',
+      author='Nathaniel Tucker',
       url='http://bitbucket.org/ntucker/django-recaptcha',
       packages=['recaptcha_form', 'recaptcha_form.registration_backend'],
      )
